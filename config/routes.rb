7986fabed2828a3389fe247bb2ccc4e7a5668b24
Justine Kenyansa <kenyansajustine@gmail.com>
2022-08-03 14:21:19 +0300

    update the routes.rb with *path thing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#index"
-  
+
+  namespace :api do
+    namespace :v1 do
+      resources :projects, params::slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all #it routes all requests that aren't for existing paths predifined in our API back to the index path. 
 end
