35b26dc8830f06985e044a624f247a6a923b7373
Justine Kenyansa <kenyansajustine@gmail.com>
2022-08-03 07:47:03 +0300

    add slugify and avg score methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,14 @@
 class Project < ApplicationRecord
+    has_many :reviews
+
+    before_create :slugify
+
+    def slugify
+        self.slug = name.parameterize
+    end
+
+    def avg_score
+        reviews.average(:sore).round(2).to_f
+    end
+    
 end
