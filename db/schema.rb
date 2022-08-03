a6dad707a2f8a8a33382d4c0156a73aab0c40025
Justine Kenyansa <kenyansajustine@gmail.com>
2022-08-03 07:36:16 +0300

    add slug column as a unique identifier for projects web address at the end of the URL

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_033857) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_043306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_033857) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "reviews", force: :cascade do |t|
